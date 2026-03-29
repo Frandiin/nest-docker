1c06ee9d5addfd69e98a3292d38775dff8fd5c60
frandin <wellingtonfrandin@hotmail.com>
2026-03-29 15:45:24 -0300

    fix:pipiline e sh

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,8 @@ RUN pnpm install
 
 COPY . .
 
-# 1 - gerar Prisma Client ANTES do build
 RUN npx prisma generate
 
-RUN npx prisma migrate deploy
-
-# 2 - build do Nest
 RUN pnpm build
 
-CMD ["pnpm", "start:prod"]
+CMD ["sh", "start.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 set -e
 
-# Espera pelo Postgres (simples loop)
 echo "Waiting for postgres..."
-until nc -z -v -w30 $(echo $DATABASE_URL | sed -n 's/.*@\(.*\):\(.*\)\/.*/\1/p') 5432
-do
+
+# extrai host do DATABASE_URL corretamente
+DB_HOST=$(echo $DATABASE_URL | sed -n 's/.*@\([^:]*\):.*/\1/p')
+
+until nc -z $DB_HOST 5432; do
   echo "Postgres unavailable - sleeping"
-  sleep 1
+  sleep 2
 done
 
 echo "Postgres is up - running migrations"
-# aplica migrações pendentes de forma segura (deploy -> para produção).
+
 npx prisma migrate deploy
 
 echo "Starting app"
+
 node dist/main.js
